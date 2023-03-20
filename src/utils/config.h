c99495d16347991178d682039ee70c26713b676e
Lygaen <smalahcayeux@gmail.com>
2023-03-20 18:49:12 +0100

    🎋 Added documentation everywhere !

--- src/utils/config.h
+++ src/utils/config.h
@@ -1,9 +1,28 @@
+/**
+ * @file config.h
+ * @author Mathieu Cayeux
+ * @brief The main file for the config io
+ * @version 1.0
+ * @date 2023-03-20
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
 #ifndef MINESERVER_CONFIG_H
 #define MINESERVER_CONFIG_H
 
 #include <memory>
 #include <rapidjson/document.h>
 
+/**
+ * @brief The Field Object for the ::Config
+ *
+ * The field object should only be used internally,
+ * as it is only used in ::Config for parsing / writing
+ * values to the config file.
+ * @tparam T the value type of the field
+ */
 template<typename T>
 class Field {
 private:
@@ -14,37 +33,138 @@ private:
     inline rapidjson::Document::ConstMemberIterator canSafelyRead(const rapidjson::Document &document);
     inline void writeSafely(rapidjson::Document &document, rapidjson::Value& v);
 public:
+    /**
+     * @brief Construct a new Field object
+     *
+     * Should only be used internally, constructs a new object
+     * that will be used in the ::Config for parsing / writing
+     * values to the config file.
+     * @param section The section of the field
+     * @param key The key / name of the field
+     * @param def Its default value
+     */
     Field(const char* section, const char *key, T def);
+    /**
+     * @brief Destroy the Field object
+     */
     ~Field();
 
+    /**
+     * @brief Loads the value from the config
+     *
+     * Loads from the json document the value,
+     * parsing it.
+     * @param document The Json document
+     */
     void load(const rapidjson::Document& document);
+    /**
+     * @brief Saves value to the config
+     *
+     * Saves value to the json document.
+     * @param document The Json document
+     */
     void save(rapidjson::Document& document);
 
+    /**
+     * @brief Get the Value of the field
+     *
+     * @return const T& the type of the field
+     */
     const T &getValue() {
         return value;
     }
 
-    T &setValue(T v) {
+    /**
+     * @brief Set the Value of the field
+     *
+     * @param v the new value
+     */
+    void setValue(T v)
+    {
         value = v;
     }
 };
 
+/**
+ * @brief The config class
+ *
+ * The (uselessly big) class that take care
+ * of the loading, saving of the configuration
+ * values from the config file.
+ * The config is a singleton.
+ */
 class Config {
 private:
     static Config *INSTANCE;
 public:
+    /**
+     * @brief Construct a new Config object
+     *
+     * Should only be used once, in main.
+     * Creating other objects will be useless as
+     * they will not be initialized.
+     */
     Config();
+    /**
+     * @brief Destroy the Config object
+     *
+     */
     ~Config();
 
+    /**
+     * @brief Saves the config on disk
+     *
+     * Writes the content of the fields on disk, saving
+     * all of it !
+     */
     void save();
+    /**
+     * @brief Loads the config from disk
+     *
+     * Loads the config by reading the file on
+     * disk as a Json Object and loading them
+     * into the fields.
+     */
     void load();
 
+    /**
+     * @brief The port of the instance
+     *
+     * The port on which the server should
+     * be listening on.
+     */
     Field<int> PORT = Field("network", "port", 25565);
+    /**
+     * @brief The Message of the Day
+     *
+     * The message of the day to be displayed
+     * on a status update.
+     */
     Field<std::string> MOTD = Field("display", "motd", std::string("This is the message of the day !"));
+    /**
+     * @brief The Log Level
+     *
+     * The minimum ::LogLevel that the ::logger
+     * should use.
+     */
     Field<std::string> LOGLEVEL = Field("other", "loglevel", std::string("ALL"));
 
+/**
+ * @brief List of all the config fields
+ *
+ * List of all of the above config fields, used
+ * as a handy tool to run similar functions
+ * on all of the fields by defining the UF(x) macro.
+ */
 #define CONFIG_FIELDS UF(PORT) UF(MOTD) UF(LOGLEVEL)
 
+    /**
+     * @brief Fetch the instance of the config
+     *
+     * Gets the instance of the config, following
+     * the singleton class-like.
+     * @return ::Config* The instance of the config
+     */
     static Config* inst() {
         return INSTANCE;
     }
--- src/utils/logger.h
+++ src/utils/logger.h
@@ -1,3 +1,14 @@
+/**
+ * @file logger.h
+ * @author Mathieu Cayeux
+ * @brief The main file for the logger
+ * @version 1.0
+ * @date 2023-03-20
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
 #ifndef MINESERVER_LOGGER_H
 #define MINESERVER_LOGGER_H
 
@@ -23,112 +34,153 @@
 #define DEBUG_COLOR ""
 #endif
 
+/**
+ * @brief The LogLevel used internally by the logger.
+ *
+ * The standard log levels in an enum, only used internally
+ * by the logger.
+ */
 enum LogLevel
 {
+    /**
+     * @brief All level, just for enabling
+     *
+     * Enables all possible log levels
+     */
     ALL = 0,
+    /**
+     * @brief Debug level
+     *
+     * see logger::debug() for more info
+     */
     DEBUG = 1,
+    /**
+     * @brief Info level
+     *
+     * see logger::info() for more info
+     */
     INFO = 2,
+    /**
+     * @brief warn level
+     *
+     * see logger::warn() for more info
+     */
     WARN = 3,
+    /**
+     * @brief Error level
+     *
+     * see logger::error() for more info
+     */
     ERROR = 4,
+    /**
+     * @brief Fatal level
+     *
+     * see logger::fatal() for more info
+     */
     FATAL = 5,
+    /**
+     * @brief Off level, just for disabling
+     *
+     * Disbles all possible log levels
+     */
     OFF = 6,
 };
 
-namespace logger
+/**
+ * @brief The logging static class
+ *
+ * The logging class, should NOT be manually
+ * created or anything. It is fully static.
+ */
+class logger
 {
-    static LogLevel LOGLEVEL = LogLevel::ALL;
+public:
+    /**
+     * @brief The LogLevel for the logger to use.
+     *
+     * The minimum log level of the logger.
+     */
+    static LogLevel LOGLEVEL;
 
-    inline std::string getTime()
-    {
-        std::time_t now = std::time(nullptr);
-        std::tm *t = std::localtime(&now);
-        char buf[80];
-        std::strftime(buf, 80, "%d/%m %X", t);
-        return buf;
-    }
+    /**
+     * @brief Get the Time as a string.
+     *
+     * Gets the local time and returns a string representation of the time accompanied
+     * by the date and the month. Used for the console logging.
+     * @return std::string the ascii representation of the local time as "day/month 24:60:60"
+     */
+    static std::string getTime();
 
-    inline void loadConfig()
-    {
-        std::string lvl = Config::inst()->LOGLEVEL.getValue();
+    /**
+     * @brief Loads the loglevel from the config.
+     *
+     * Loads the loglevel from the config and parse it to be usable,
+     * should only be used internally as a hook after the config
+     * was loaded, which is in Config::load().
+     */
+    static void loadConfig();
 
-#define QCK(X)      \
-    if (lvl == "X") \
-    LOGLEVEL = LogLevel::X
-        QCK(ALL);
-        QCK(DEBUG);
-        QCK(INFO);
-        QCK(WARN);
-        QCK(ERROR);
-        QCK(FATAL);
-        QCK(OFF);
-#undef QCK
-    }
+    /**
+     * @brief Logs something at the ::DEBUG level
+     *
+     * In the end is just a wrapper around printf, so it
+     * will works like it.
+     * Should be used to log information only used by developers.
+     * @param format the format string to parse arguements in
+     * @param ... the arguments for the format
+     */
+    static void debug(const char *format, ...);
 
-    inline void debug(const char *format, ...)
-    {
-        if (LogLevel::DEBUG < LOGLEVEL)
-            return;
+    /**
+     * @brief Logs something at the ::INFO level
+     *
+     * In the end is just a wrapper around printf, so it
+     * will works like it.
+     * Should be used to print out general information
+     * such as the current status of the program and
+     * so on and so on.
+     * @param format the format string to parse arguements in
+     * @param ... the arguments for the format
+     */
+    static void info(const char *format, ...);
 
-        va_list args;
-        va_start(args, format);
+    /**
+     * @brief Logs something at the ::WARN level
+     *
+     * In the end is just a wrapper around printf, so it
+     * will works like it.
+     * Should be used to log information that the end user
+     * should be warned about, but has no impact.
+     * @param format the format string to parse arguements in
+     * @param ... the arguments for the format
+     */
+    static void warn(const char *format, ...);
 
-        std::string formatted = DEBUG_COLOR + std::string("[DEBUG] ") + TIME_COLOR + getTime() + RESET_COLOR + " - " + format + "\n";
+    /**
+     * @brief Logs something at the ::ERROR level
+     *
+     * In the end is just a wrapper around printf, so it
+     * will works like it.
+     * Should only be used to logs errors to the end user,
+     * when the program has done / detected something wrong,
+     * that impacts the end user.
+     * @param format the format string to parse arguements in
+     * @param ... the arguments for the format
+     */
+    static void error(const char *format, ...);
 
-        std::vprintf(formatted.c_str(), args);
-        va_end(args);
-    }
-
-    inline void info(const char *format, ...)
-    {
-        if (LogLevel::INFO < LOGLEVEL)
-            return;
-
-        va_list args;
-        va_start(args, format);
-
-        std::string formatted = INFO_COLOR + std::string("[INFO]  ") + TIME_COLOR + getTime() + RESET_COLOR + " - " + format + "\n";
-        std::vprintf(formatted.c_str(), args);
-        va_end(args);
-    }
-
-    inline void warn(const char *format, ...)
-    {
-        if (LogLevel::WARN < LOGLEVEL)
-            return;
-
-        va_list args;
-        va_start(args, format);
-
-        std::string formatted = WARN_COLOR + std::string("[WARN]  ") + TIME_COLOR + getTime() + RESET_COLOR + " - " + format + "\n";
-        std::vprintf(formatted.c_str(), args);
-        va_end(args);
-    }
-
-    inline void error(const char *format, ...)
-    {
-        if (LogLevel::ERROR < LOGLEVEL)
-            return;
-
-        va_list args;
-        va_start(args, format);
-
-        std::string formatted = ERROR_COLOR + std::string("[ERROR] ") + TIME_COLOR + getTime() + RESET_COLOR + " - " + format + "\n";
-        std::vprintf(formatted.c_str(), args);
-        va_end(args);
-    }
-
-    inline void fatal(const char *format, ...)
-    {
-        if (LogLevel::FATAL < LOGLEVEL)
-            return;
-
-        va_list args;
-        va_start(args, format);
-
-        std::string formatted = FATAL_COLOR + std::string("[FATAL] ") + TIME_COLOR + getTime() + RESET_COLOR + " - " + format + "\n";
-        std::vprintf(formatted.c_str(), args);
-        va_end(args);
-    }
-}
+    /**
+     * @brief Logs something at the ::FATAL level
+     *
+     * In the end is just a wrapper around printf, so it
+     * will works like it.
+     * Should only be used when a fatal error has happened,
+     * that crashes the program or has a severe impact for
+     * the well-being of the program.
+     * @param format the format string to parse arguements in
+     * @param ... the arguments for the format
+     */
+    static void fatal(const char *format, ...);
+};
 
 #endif // MINESERVER_LOGGER_H
