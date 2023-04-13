a60000062e52b9c59b45324baa03454fef9e0159
Lygaen <smalahcayeux@gmail.com>
2023-04-13 19:27:54 +0200

    🌐 Added a main page bc it was sooo boring

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,96 @@
+/**
+ * @mainpage FAQ
+ *
+ * A rewrite of Minecraft 1.8.9 in C++ !
+ * It is fricking fast with multi-threading, and other
+ * amazing features and oprimizations.
+ * Informations such as installation, contributing
+ * can be found on [the Github's page](https://github.com/Lygaen/mineserver).
+ * Here's a quick FAQ.
+ *
+ * @section why_another_server WHY another server ?
+ * We created another server library because of a small
+ * side project and because we felt the need to create
+ * something to replace all the bloaty-servers Bukkit-based
+ * that are way too inefficient. Even though PaperMC tries
+ * to tackle that by removing the slack of Bukkit, it is
+ * still way too slow compared to other game's current
+ * standards.
+ *
+ * @section why_cpp Why bother using C++ ?
+ * Mineserver, in contrast to Mojang's java build, aims
+ * to be *blazingly* fast so that it entirely surpasses
+ * other implementations such as Paper and other. We
+ * could rewrite the server in Java, but, in the end,
+ * using a low-level programming does help the work.
+ *
+ * @section why_opensource Why making it Open-Source ?
+ * We made it open source mainly because we want to
+ * share this project and because we don't feel the need
+ * to hide it. We want to help inspire many programmers
+ * that I hope will one day read over this project because
+ * it crosses a lot of programmation concepts.
+ * That's why the code used here should be reusable, readable and
+ * fully documented. For documentation, we use doxygen
+ * which you can see in the bottom-right corner.
+ * This is not the default skin of doyxgen though.
+ *
+ * @section why_version Why 1.8.9 ?
+ * We also aim for stability, and to help us for that
+ * we only target Minecraft 1.8.9 (because it is used
+ * a lot). Of course, when the project extends in the future,
+ * we aim to support more and more versions. But for the
+ * time being, because we want first to create a strong
+ * architecture, we only support 1.8.9 and theoratically
+ * all 1.8 versions.
+ *
+ * @section what_features What features do you have compared to other ?
+ * We have several features :
+ *  - We try to be as time-efficient as possible
+ *  - We try to be very memory-efficient, no need for 2Gb
+ *    of RAM anymore just for 2-players play
+ *  - We added fricking **multithreading** which is a
+ *    late addition to Minecraft if we compare it to other games,
+ *    no need for a godly CPU anymore !
+ *  - And many more... (Remember that this is only the start
+ *    of the project)
+ *
+ * @section why_so_long Why does the project takes so much time ?
+ * This project takes *sooooo* much time because of its nature.
+ * Because we decided to write it in C++ to leverage its low-levelness,
+ * memory-efficiency and speed. But with great power comes great responsability.
+ * Because of C++ and the way it was designed, it take way more time to
+ * design project using it ! And finally its not like we have 200+
+ * peoples behind keyboards to code this project.
+ *
+ * @section faq_end_note End note with a touch of existential crisis
+ * If you read all of this (you liar) well done ! If you want
+ * to ask other questions that are not on this page, ask
+ * using [Github's issues system](https://github.com/Lygaen/mineserver/issues).
+ *
+ * I (Lygaen) used "we" throughout this FAQ, but I am very alone to
+ * create this project. That's why any help, even from beginners
+ * is welcomed ! I'll always be happy to help you guys
+ * in any way. I don't think that anyone will be reading this
+ * but anyway ! It's like I'm having a existense crisis on
+ * the keyboard, it is very weird...
+ *
+ * Nevertheless, I hope that you guys will have a great day (and
+ * go outside for god's sake!) and that reading these small
+ * paragraphs on why I created / still continue to work on
+ * this project will give you a bit of hope of what the open-source
+ * community is able to do with a bit of time. It will be important
+ * that you keep this feeling intact and maybe pass it to another
+ * person later, it might greatly help them in some way or
+ * another...
+ *
+ *
+ * PS : English is not my main language so all of it might feel
+ * weird to read at first, but I assure you that I'm trying
+ * my best to give you my good vibes and not trying to pluck
+ * your eyeballs !
+ */
+
 #include <iostream>
 #include <utils/config.h>
 #include <server.h>
