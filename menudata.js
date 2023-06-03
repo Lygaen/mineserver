3f3b796cfbcb825e806cf86aaaa5f272c360120c
Lygaen <Lygaen@users.noreply.github.com>
2023-06-03 17:29:08 +0000

    🚀 Updated everything accordingly e1b991baab30fdb92733d419381ae386805ba445

--- menudata.js
+++ menudata.js
@@ -75,7 +75,7 @@ var menudata={children:[
 {text:"z",url:"functions_func.html#index_z"},
 {text:"~",url:"functions_func.html#index__7E"}]},
 {text:"Variables",url:"functions_vars.html"},
-{text:"Related Functions",url:"functions_rela.html"}]}]},
+{text:"Related Symbols",url:"functions_rela.html"}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
