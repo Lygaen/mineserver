0c1b6cbedce9c0146d76bf5935993d863afb3ac1
Lygaen <Lygaen@users.noreply.github.com>
2024-05-06 07:46:29 +0000

    🥳 Fixed support for MSVC fdfd14c4795610b1c57b586869e4fad6eba6f85f

--- classEventsManager.js
+++ classEventsManager.js
@@ -4,7 +4,7 @@ var classEventsManager =
     [ "~EventsManager", "classEventsManager.html#a536b233bc47fa1409efa1106403f9043", null ],
     [ "fire", "classEventsManager.html#ac2e3dcab35985173976477a9ea8545c8", null ],
     [ "inst", "classEventsManager.html#a8b6868e7bf770c95e5706f4cf817bf56", null ],
-    [ "subscribe", "classEventsManager.html#a17a034b18a623bb8db75bf16aabeb7c0", null ],
-    [ "subscribe", "classEventsManager.html#abd6add9e9a5cfc2ec483208a8ef0f603", null ],
-    [ "unsubscribe", "classEventsManager.html#a502a3de0687be48eb8d52c0bf2525ed2", null ]
+    [ "subscribe", "classEventsManager.html#ad3bafbcc80b45c43c4fdd7657eec32a5", null ],
+    [ "subscribe", "classEventsManager.html#ab788ade250b06f5fad2677f9806cbd72", null ],
+    [ "unsubscribe", "classEventsManager.html#aa28b8016df21d938a212cccdc98e1c37", null ]
 ];
--- search/functions_11.js
+++ search/functions_11.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['uncompress_0',['uncompress',['../classcrypto_1_1ZLibCompressor.html#a4e042327c18c77272c53c092ec20c968',1,'crypto::ZLibCompressor']]],
-  ['unsubscribe_1',['unsubscribe',['../classEventHandler.html#a81d0ad307f791ef7f4fa8a4387812c0e',1,'EventHandler::unsubscribe()'],['../classEventsManager.html#a502a3de0687be48eb8d52c0bf2525ed2',1,'EventsManager::unsubscribe()']]],
+  ['unsubscribe_1',['unsubscribe',['../classEventHandler.html#a81d0ad307f791ef7f4fa8a4387812c0e',1,'EventHandler::unsubscribe()'],['../classEventsManager.html#aa28b8016df21d938a212cccdc98e1c37',1,'EventsManager::unsubscribe()']]],
   ['update_2',['update',['../classcrypto_1_1MinecraftHash.html#a86172a5bbf208b64873a6a54bdebdc9f',1,'crypto::MinecraftHash::update()'],['../classcrypto_1_1AES128CFB8Cipher.html#a4a63c03f53297285956bc2c2652f3b1d',1,'crypto::AES128CFB8Cipher::update()']]]
 ];
